ae59496a3ea00ba14d2169e401d20faa0472c21b
Karen V <karenvo@usc.edu>
2019-02-08 15:46:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,13 @@ end
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty"
-    ...
+  else
+    number = 1
+    katz_deli. each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
 end
 
 def now_serving ()
