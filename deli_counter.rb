934eb8e89c12eac33ab7a4e39d0cb6cbc27418ce
Karen V <karenvo@usc.edu>
2019-02-08 15:45:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,8 +11,8 @@ def take_a_number(katz_deli, name)
 end
 
 def line(katz_deli)
-  if katz_deli == 0
-    return "The line is currently empty"
+  if katz_deli.empty?
+    puts "The line is currently empty"
     ...
 end
 
