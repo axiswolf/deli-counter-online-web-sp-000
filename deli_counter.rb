85703c373cdabc2961959fde592171370ed70c62
Karen V <karenvo@usc.edu>
2019-02-08 15:48:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,8 +12,8 @@ def line(katz_deli)
     katz_deli. each do |customer|
       line_positions << "#{number}. #{customer}"
       number += 1
-    end
-    puts "The line is currently: #{line_positions.join(" ")}"
+  end
+  puts "The line is currently: #{line_positions.join(" ")}"
 end
 
 def now_serving ()
