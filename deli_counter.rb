2d7b53ed637e52a496c9f9943321ae0f1fbe2cb7
Karen V <karenvo@usc.edu>
2019-02-08 15:51:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,5 @@
 # Write your code here.
 katz_deli = []
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line"
-end
-
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty"
@@ -15,6 +10,10 @@ def line(katz_deli)
     number += 1
   end
   puts "The line is currently: #{line_positions.join(" ")}"
+end
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line"
 end
 
 def now_serving (katz_deli)
